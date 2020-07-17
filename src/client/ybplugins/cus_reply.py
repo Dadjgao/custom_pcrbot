3947b7f6f0673d0b6b1f8977eee65b7186089936
Dadjgao <39488442+Dadjgao@users.noreply.github.com>
2020-07-17 14:25:56 +0800

    update

--- src/client/ybplugins/cus_reply.py
+++ src/client/ybplugins/cus_reply.py
@@ -49,14 +49,14 @@ class Cus_reply:
 
         reply = None
         # 设置回复文本
-        if (re.match(r'^[(镜华)(XCW)(xcw)]', msg)):
+        if msg == '镜华' or msg == 'xcw' or msg == 'XCW':
             reply = ['变态！叫我干嘛！爬爬爬', 
                      '呐，你叫我是想干什么呢？', 
                      '镜华累了，镜华不想说话。',
                      '作业做完了吗？我才不来帮你做作业！',
                      ][random.randint(0,3)]
 
-        elif (re.match(r'^[(小狐狸)(fbk)(FBK)]', msg)):
+        elif msg == '小狐狸' or msg == 'fbk' or msg == 'FBK':
             reply = [
                      '你又在想别的女人了！',
                      'FBK斯哈斯哈！！昆卡昆卡',
