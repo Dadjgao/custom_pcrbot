d1b4436af4922ecf12cd956b455ac2b2dfdfd138
Dadjgao <39488442+Dadjgao@users.noreply.github.com>
2020-07-18 12:00:36 +0800

    update conversition

--- src/client/ybplugins/cus_reply.py
+++ src/client/ybplugins/cus_reply.py
@@ -50,14 +50,16 @@ class Cus_reply:
 
         reply = None
         # 设置回复文本
-        if msg == '镜华' or msg == 'xcw' or msg == 'XCW':
+        if (re.match(r'^[(镜华)(xcw)(XCW)][\S]?[\S]?', msg))
+        # if msg == '镜华' or msg == 'xcw' or msg == 'XCW':
             reply = ['变态！叫我干嘛！爬爬爬', 
                      '呐，你叫我是想干什么呢？', 
                      '镜华累了，镜华不想说话。',
                      '作业做完了吗？我才不来帮你做作业！',
-                     ][random.randint(0,3)]
+                     '又想玩捉迷藏了？',
+                     ][random.randint(0,4)]
 
-        elif msg == '小狐狸' or msg == 'fbk' or msg == 'FBK':
+        elif (re.match(r'^[(小狐狸)(fbk)(FBK)][\S]?[\S]?', msg)):
             reply = [
                      '你又在想别的女人了！',
                      'FBK斯哈斯哈！！昆卡昆卡',
@@ -65,11 +67,14 @@ class Cus_reply:
                      '本群唯一指定单推小狐狸！',
                     ][random.randint(0,3)]
 
-        elif (re.match(r'^[(爬)(爪巴)][\S]*', msg)):
+        elif (re.match(r'^(给爷)?[(爬)(爪巴)][\S]*', msg)):
             reply = '[CQ:at,qq={}] 你才爪巴！'.format(UID)
             
         elif re.match(r'^镜华是谁[\S]?[\S]?', msg):
             reply = '[CQ:at,qq={}] 镜华是本馆最可爱的女仆~'.format(UID)
+            
+        elif re.match(r'^[\S]*斯哈斯哈[\S]*$', msg):
+            reply = '[CQ:at,qq={}] 不许斯哈~'.format(UID)
 
         else:
             return False
