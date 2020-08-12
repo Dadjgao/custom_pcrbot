f7d7a6edb219943889094f94c32426f4c4cda1d7
Dadjgao <39488442+Dadjgao@users.noreply.github.com>
2020-08-12 13:52:01 +0800

    add rr

--- src/client/ybplugins/cus_reply.py
+++ src/client/ybplugins/cus_reply.py
@@ -28,7 +28,38 @@ def dice():
     return random.randint(1, 6)
 
 
+def russian_roll(in_roll_user_UID, msgs_after_begin, flag, UID=None, in_roll = 5):
+    if UID == None: return None
+    
+    msg = ''
+
+    if len(in_roll_user) >= in_roll:  # 结束
+        msg = '[CQ:at,qq={}] 你马没了'.format(in_roll_user_UID[random.randint(0, in_roll-1))
+
+        in_roll_user_UID.clear()
+        msgs_after_begin = 0
+        flag = 0
+        
+    
+    elif msgs_after_begin >= 20:  # 消息数量超过结束
+        msg = '[CQ:at,qq={}] 你马没了'.format(in_roll_user_UID[random.randint(0, len(in_roll_user_UID)))
+
+        in_roll_user_UID.clear()
+        msgs_after_begin = 0
+        flag = 0
+    
+    else:
+        in_roll_user_UID.append(UID)
+        msg = '[CQ:at,qq={}] 你的马已经被塞到转盘里了'.format(UID)
+
+    return msg
+
+
 class Cus_reply:
+    in_roll_user_UID = []
+    buffed_msgs = 0
+    flag = 0
+
     def __init__(self,
                  glo_setting: Dict[str, Any],
                  scheduler: AsyncIOScheduler,
@@ -42,6 +73,13 @@ class Cus_reply:
         self.setting = glo_setting
         self.api = bot_api
 
+
+    def reset_status(self):
+        self.flag = 0
+        self.in_roll_user_UID.clear()
+        self.buffed_msgs = 0
+
+
     async def execute_async(self, ctx: Dict[str, Any]) -> Union[None, bool, str]:
 
         MsgType = ctx['message_type']
@@ -50,22 +88,15 @@ class Cus_reply:
         if MsgType == "group":
             GID = ctx['group_id']
             Role = ctx['sender']['role']
+        
+        if self.buffed_msgs < 20: 
+            self.buffed_msgs += 1
+        else:
+            self.buffed_msgs = 1
 
         reply = None
         # 设置回复文本
-        if (re.match(r'^镜华[\S]?$', msg)):
-        # if msg == '镜华' or msg == 'xcw' or msg == 'XCW':
-            reply = ['变态！叫我干嘛！爬爬爬', 
-                     '呐，你叫我是想干什么呢？', 
-                     '镜华累了，镜华不想说话。',
-                     '作业做完了吗？我才不来帮你做作业！',
-                     '又想玩捉迷藏了？',
-                     ][random.randint(0,4)]
-
-        elif (re.match(r'^(给爷)?[(爬)(爪巴)][\S]*', msg)):
-            reply = '[CQ:at,qq={}] 你才爪巴！'.format(UID)
-            
-        elif re.match(r'^镜华是谁[\S]?[\S]?', msg):
+        if re.match(r'^镜华是谁[\S]?[\S]?', msg):
             reply = '[CQ:at,qq={}] 镜华是本群最可爱的{}~'.format(UID, 
                                                                ['机器人', '女仆'][random.randint(0,1)])
             
@@ -75,6 +106,16 @@ class Cus_reply:
         elif re.match(r'^(色子|骰子)$', msg):
             dice_res = dice()
             reply = '[CQ:at,qq={}] 本次投掷结果为{}'.format(UID, dice_res)
+
+        # 俄罗斯转盘
+        elif msg == '俄罗斯转盘' and self.flag == 0:
+            self.reset_status()
+
+            self.flag = 1  # 开始游戏
+            reply = russian_roll(self.in_roll_user_UID, self.buffed_msgs, self.flag, UID)
+        
+        elif msg == '参加' and self.flag == 1:
+            reply = russian_roll(self.in_roll_user_UID, self.buffed_msgs, self.flag, UID)
 
         else:
             return False
